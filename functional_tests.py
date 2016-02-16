04a207b53497f63d8b34f007a169546231968ebf
tareqobaida <tareqbuet@gmail.com>
2016-02-16 16:22:20 -0600

    redirect after post and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -44,6 +44,7 @@ class NewVisitorTest(unittest.TestCase):
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
--- lists/views.py
+++ lists/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
 
 
 # Create your views here.
 def home_page(request):
     if request.method == 'POST':
-        item = Item()
-        item.text = request.POST['item_text']
-        item.save()
-        return render(request, 'home.html', context={'new_item_text': request.POST['item_text']})
-    else:
-        return render(request, 'home.html')
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text= new_item_text)
+        return redirect('/')
+    items = Item.objects.all()
+    return render(request, 'home.html', context={
+        'items': items,
+    })
