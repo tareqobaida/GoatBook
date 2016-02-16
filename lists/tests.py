da399beff52412b2c06c30a1da24007f9c0c038c
Obaida <abu.obaida@austin.utexas.edu>
2016-02-16 11:57:36 -0600

    ItemModelTest created

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,23 @@
+import unittest
 from django.test import TestCase
+from lists.models import Item
 
 # Create your tests here.
-class SmokeTest(TestCase):
-	def test_bad_maths(self):
-		self.assertEqual(1,3)
-		
+class ItemModelTest(TestCase):
+	def test_saving_and_retrieving_items(self):
+		first_item=Item()
+		first_item.text='The First list item'
+		first_item.save()
+		second_item=Item()
+		second_item.text='Item the second'
+		second_item.save()
+		saved_items=Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+		first_saved_item=saved_items[0]
+		second_saved_item=saved_items[1]
+		self.assertEqual(first_saved_item.text,'The First list item' )
+		self.assertEqual(second_saved_item.text,'Item the second' )
+
+
+if __name__ == '__main__':
+    unittest.main()
