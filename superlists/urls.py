a1499bb6a2c9802daeaa1efb9b37a48a493aaf4e
tareqobaida <tareqbuet@gmail.com>
2016-02-19 15:39:05 -0600

    upto chap 6 a final refactor using url includes

--- superlists/urls.py
+++ superlists/urls.py
@@ -17,9 +17,11 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from lists import views
 
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.home_page, name= 'home page'),
-    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name= 'view_list'),
+    url(r'^lists/(\d+)/$', views.view_list, name= 'view_list'),
+    url(r'^lists/(\d+)/add_item$', views.add_item, name= 'add_item'),
     url(r'^lists/new$', views.new_list, name= 'new_list'),
 ]
