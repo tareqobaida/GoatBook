19b0af5aa94eb08b7e5b45f57ead574786c3b652
abu <tareqbuet@gmail.com>
2016-03-01 00:37:41 -0600

    ch11

---
+++ lists/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Item
+
+
+class ItemForm(forms.models.ModelForm):
+
+    class Meta:
+        model = Item
+        fields = ('text',)
+        widgets = {
+            'text': forms.fields.TextInput(attrs={
+                'placeholder': 'Enter a to-do item',
+                'class': 'form-control input-lg',
+            }),
+        }
